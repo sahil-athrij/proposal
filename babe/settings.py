1027284d22daad61266d94cfc4197a3b2b6a895d
Sahil Athrij <sahilathrij@gmail.com>
2019-04-22 07:09:22 +0530

    initial

--- babe/settings.py
+++ babe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '(6e*h__0id2z)h+$wsd#8jefht+si7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', True) != 'False'
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1","infinite-island-28522.herokuapp.com"]
 
 # Application definition
 
@@ -119,10 +119,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'photo/')
 
 MEDIA_URL = "photo/"
